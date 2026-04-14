4de5e104bbc2a2f5754fae7ab97a9ed1ade0da56
xieby1 <xieby1@outlook.com>
2026-04-14 15:54:21 +0800

    feat(shell.nix): add nixpkgs to NIX_PATH to make `nix-shell -p` work

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 let
-  pkgs = import (import ./npins).nixpkgs {};
+  npinsed = import ./npins;
+  pkgs = import npinsed.nixpkgs {};
 in pkgs.mkShell {
   name = "verilua-nix";
   packages = [
     pkgs.npins
   ];
   shellHook = ''
-    export NIX_PATH=npins=$(realpath ./npins/):luajit-pro=$(realpath ./luajit-pro/);
+    export NIX_PATH=nixpkgs=${npinsed.nixpkgs}:npins=$(realpath ./npins/):luajit-pro=$(realpath ./luajit-pro/);
   '';
 }
