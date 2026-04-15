7287f31213342224fd54a6ea71da3274bd6623d2
xieby1 <xieby1@outlook.com>
2026-04-15 09:26:28 +0800

    feat(lua-modules): add verilua-src-lua

---
+++ lib/lua-modules/verilua-src-lua/default.nix
@@ -0,0 +1,10 @@
+# TODO: this file is redundant as we already have ../thirdparty_lib and ../verilua
+let
+  luaPackages = (import <luajit-pro>).pkgs;
+  npinsed = import <npins>;
+  pkgs = import npinsed.nixpkgs {};
+  inherit (luaPackages.lua) luaversion;
+in luaPackages.toLuaModule (pkgs.runCommand "lua${luaversion}-verilua-src-lua" {} ''
+  mkdir -p $out/share/lua/${luaversion}
+  cp -sr ${npinsed.verilua + /src/lua}/* $out/share/lua/${luaversion}
+'')
---
+++ lib/lua-modules/verilua-src-lua/test.nix
@@ -0,0 +1,39 @@
+let
+  npinsed = import <npins>;
+  pkgs = import npinsed.nixpkgs {};
+  luajit-pro = import <luajit-pro>;
+  luaPackages = luajit-pro.pkgs;
+  inherit (luaPackages.lua) luaversion;
+  dut = import ./.;
+in pkgs.lib.runTests {
+  # Test main Lua files exist
+  test-main-files = pkgs.lib.testAllTrue [
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/Verilua.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/init.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/meta.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/strict.lua))
+  ];
+
+  # Test key module files exist
+  test-key-modules = pkgs.lib.testAllTrue [
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/LuaDut.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/LuaSimulator.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/LuaUtils.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/TccWrapper.lua))
+  ];
+
+  # Test subdirectory module files exist
+  test-subdir-modules = pkgs.lib.testAllTrue [
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/utils/BitVec.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/scheduler/LuaScheduler.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/handles/LuaBundle.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/verilua/coverage/CoverGroup.lua))
+  ];
+
+  # Test thirdparty_lib files exist
+  test-thirdparty-lib = pkgs.lib.testAllTrue [
+    (builtins.pathExists (dut + /share/lua/${luaversion}/thirdparty_lib/json.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/thirdparty_lib/inspect.lua))
+    (builtins.pathExists (dut + /share/lua/${luaversion}/thirdparty_lib/fun.lua))
+  ];
+}
