b8855ab787255c82d786f5fe828c14c81067548b
xieby1 <xieby1@outlook.com>
2026-04-16 09:28:33 +0800

    fix(lua-modules): use symlinks instead of copies for verilua lua files

--- lib/lua-modules/verilua/default.nix
+++ lib/lua-modules/verilua/default.nix
@@ -5,5 +5,5 @@ let
   inherit (luaPackages.lua) luaversion;
 in luaPackages.toLuaModule (pkgs.runCommand "lua${luaversion}-verilua" {} ''
   mkdir -p $out/share/lua/${luaversion}
-  cp -r ${npinsed.verilua}/src/lua/verilua/* $out/share/lua/${luaversion}
+  cp -sr ${npinsed.verilua}/src/lua/verilua/* $out/share/lua/${luaversion}
 '')
