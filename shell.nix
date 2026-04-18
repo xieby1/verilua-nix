da1df2343a2d10f479aed87fe2ba51b21a2ff237
xieby1 <xieby1@outlook.com>
2026-04-18 14:18:25 +0800

    feat: add dolt

--- shell.nix
+++ shell.nix
@@ -5,5 +5,10 @@ in pkgs.mkShell {
   name = "verilua-nix";
   packages = [
     pkgs.npins
+    # Dolt >= v1.81.10 supports git remotes.
+    # The version in nixpkgs 25.11 too old,
+    # so we use dolt from nixpkgs-unstable instead.
+    # See: https://www.dolthub.com/blog/2026-02-13-announcing-git-remote-support-in-dolt/
+    (import npinsed.nixpkgs-unstable {}).dolt
   ];
 }
